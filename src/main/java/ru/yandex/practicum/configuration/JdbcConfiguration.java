4146f82d1b3004d6374076fe40e35c417e207023
Ivan Busygin <busygin_02@list.ru>
2026-01-08 11:36:39 +0100

    JDBC configuration

---
+++ src/main/java/ru/yandex/practicum/configuration/JdbcConfiguration.java
@@ -0,0 +1,32 @@
+package ru.yandex.practicum.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class JdbcConfiguration {
+
+    @Bean
+    public DataSource dataSource(
+            @Value("${db.url}") String url,
+            @Value("${db.username}") String username,
+            @Value("${db.password}") String password
+    ) {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("org.postgresql.Driver");
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        return ds;
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+}

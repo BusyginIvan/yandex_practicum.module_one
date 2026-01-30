5f592228011c8e1349368217b467e3d446fd9c34
Ivan Busygin <busygin_02@list.ru>
2026-01-30 16:32:15 +0100

    Spring Boot + Gradle migration

--- src/test/java/ru/yandex/practicum/api/AbstractApiTest.java
+++ src/test/java/ru/yandex/practicum/api/AbstractApiTest.java
@@ -1,37 +1,25 @@
 package ru.yandex.practicum.api;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
 import ru.yandex.practicum.configuration.ApiTestConfiguration;
 import ru.yandex.practicum.service.CommentService;
 import ru.yandex.practicum.service.PostImageService;
 import ru.yandex.practicum.service.PostService;
+import tools.jackson.databind.ObjectMapper;
 
-import static org.mockito.Mockito.reset;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
-
-@WebAppConfiguration
-@SpringJUnitConfig(classes = ApiTestConfiguration.class)
+@WebMvcTest
+@Import(ApiTestConfiguration.class)
 public abstract class AbstractApiTest {
 
-    @Autowired protected WebApplicationContext wac;
-
-    @Autowired protected PostService postService;
-    @Autowired protected PostImageService postImageService;
-    @Autowired protected CommentService commentService;
-
+    @Autowired protected MockMvc mvc;
     @Autowired protected ObjectMapper objectMapper;
 
-    protected MockMvc mvc;
-
-    @BeforeEach
-    void setUpMockMvcAndResetMocks() {
-        reset(postService, postImageService, commentService);
-        this.mvc = webAppContextSetup(wac).build();
-    }
+    @MockitoBean protected PostService postService;
+    @MockitoBean protected PostImageService postImageService;
+    @MockitoBean protected CommentService commentService;
 }
+
--- src/test/java/ru/yandex/practicum/e2e/AbstractE2eTest.java
+++ src/test/java/ru/yandex/practicum/e2e/AbstractE2eTest.java
@@ -1,33 +1,30 @@
 package ru.yandex.practicum.e2e;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
-import ru.yandex.practicum.configuration.E2eTestConfiguration;
+import ru.yandex.practicum.configuration.PostgresTestConfiguration;
 import ru.yandex.practicum.storage.InMemoryPostImageStorage;
+import tools.jackson.databind.ObjectMapper;
 
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Import(PostgresTestConfiguration.class)
+public abstract class AbstractE2eTest {
 
-@WebAppConfiguration
-@SpringJUnitConfig(classes = E2eTestConfiguration.class)
-public class AbstractE2eTest {
-
-    @Autowired protected WebApplicationContext wac;
+    @Autowired protected MockMvc mvc;
     @Autowired protected ObjectMapper objectMapper;
     @Autowired protected NamedParameterJdbcTemplate jdbc;
     @Autowired protected InMemoryPostImageStorage postImageStorage;
 
-    protected MockMvc mvc;
-
-    @BeforeEach
+    @AfterEach
     void beforeEach() {
-        this.mvc = webAppContextSetup(wac).build();
-
         jdbc.getJdbcTemplate().execute("""
             TRUNCATE TABLE
                 post_tags,
